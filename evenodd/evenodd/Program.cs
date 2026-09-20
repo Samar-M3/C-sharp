e15b1113027f912b78fa58c7e55c06c78a0f505a
Samar Maharjan <samarmhrzn257@gmail.com>
2026-09-20 07:37:02 +0545

    even odd

---
+++ evenodd/evenodd/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace evenodd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter a number");
+            int number=int.Parse(Console.ReadLine());
+            if (number%2==0)
+            {
+                Console.WriteLine("the number is even");
+            }
+            else
+            {
+                Console.WriteLine("the number is odd");
+            }
+        }
+    }
+}
