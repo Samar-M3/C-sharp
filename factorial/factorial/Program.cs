ea68e6dcf24692a9f2c7b4340bbb2f4ba93b7d5f
Samar Maharjan <samarmhrzn257@gmail.com>
2026-09-20 07:38:07 +0545

    factorial

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter a non negative number to calculate its factorial");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for(int i=1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+                Console.WriteLine($"Factorial is {fact}");
+        }
+    }
+}
+
