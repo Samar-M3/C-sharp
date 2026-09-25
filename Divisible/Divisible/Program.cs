a6e0b2ba40a8d0efd485d40fe52432ce4f3f8ca0
Samar Maharjan <samarmhrzn257@gmail.com>
2026-09-25 20:32:52 +0545

    new task of dotnet

---
+++ Divisible/Divisible/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Divisible
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter a number to check if it is divisible by 2, 3, or 5:");
+            //int num=int.Parse(Console.ReadLine());
+            //if (num % 5 == 0 )
+            //{
+            //    Console.WriteLine($"the number {num} is divisible by 5 ");
+            //}
+            //else if(num % 3 == 0){
+            //    Console.WriteLine($"the number {num} is divisible by 3 ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the number is not divisible");
+            //}
+
+            Console.WriteLine("Enter something:");
+            string input = Console.ReadLine();
+
+            while (input != null)
+            {
+                Console.WriteLine($"You entered: {input}");
+                Console.WriteLine("enter Y to continue and N to exit");
+                string choice = Console.ReadLine().ToUpper();
+                if (choice == "Y")
+                {
+                    Console.WriteLine("Enter something:");
+                    input = Console.ReadLine();
+                }
+                else if (choice == "N")
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ simplecalc/simplecalc/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace simplecalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+            Console.WriteLine("choose 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
+            int choice = Convert.ToInt16(Console.ReadLine());
+            float num1, num2;
+            Console.WriteLine("enter two numbers"); 
+            num1 = float.Parse(Console.ReadLine());
+            num2 = float.Parse(Console.ReadLine());
+           
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine($"the addition of {num1} and {num2} is {num1 + num2}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine($"the subtraction of {num1} and {num2} is {num1 - num2}");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine($"the multiplication of {num1} and {num2} is {num1 * num2}");
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (num2 == 0)
+                            {
+                                Console.WriteLine("division by zero is not allowed");
+                                break;
+                            }
+                            Console.WriteLine($"the division of {num1} and {num2} is {num1 / num2}");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("invalid choice");
+                            break;
+                        }
+
+                }
+            }
+
+        }
+        }
+}
